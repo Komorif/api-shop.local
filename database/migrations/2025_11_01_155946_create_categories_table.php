6c3d1c9723e8a1a559e2c3aadc261e7a84bf853c
Komori <121684959+Komorif@users.noreply.github.com>
2025-11-07 20:21:01 +0300

    Добавление/удаление/редактирование категорий

--- database/migrations/2025_11_01_155946_create_categories_table.php
+++ database/migrations/2025_11_01_155946_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('description');
+            $table->foreignId('users')->constrained("users")->onDelete("cascade");
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -23,10 +23,15 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('categories', [CategoryController::class, 'index']); // Получения списка категорий
     Route::get('/categories/{category_id}/products', [CategoryController::class, 'show']); // Получения списка товаров категории
     
+    Route::post('categories', [CategoryController::class, 'store']); // Добавление категории
+    Route::put('categories/{category_id}', [CategoryController::class, 'update']); // Обновление категории
+    Route::delete('categories/{category_id}', [CategoryController::class, 'destroy']); // Удаление категории
+    
     Route::get('/products/{product_id}', [ProductController::class, 'index']); // Просмотр товара
-    Route::post('/products/{product_id}/buy', [ProductController::class,'store']); // Покупка товара
 
 
-    //Route::post('/payыment-webhook', [PaymentController::class,'store']); // Покупка товара
+
+    //Route::post('/products/{product_id}/buy', [ProductController::class,'store']); // Покупка товара
+    //Route::post('/payment-webhook', [PaymentController::class,'store']); // Покупка товара
     //Route::get('/orders', [OrderController::class,'store']); // Покупка товара
 });
